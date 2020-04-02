774de27aea0c3a1fa7194a233b620039712c7b88
JohnStar128 <jogbstar128@gmail.com>
2020-04-02 09:16:29 -0400

    Fix console error with message formatting

--- src/main/java/me/johnstar128/frostyssentials/events/MessageFormatting.java
+++ src/main/java/me/johnstar128/frostyssentials/events/MessageFormatting.java
@@ -1,7 +1,6 @@
 package me.johnstar128.frostyssentials.events;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -11,7 +10,6 @@ public class MessageFormatting implements Listener {
     @EventHandler
     public void onMessageSend(AsyncPlayerChatEvent e) {
         String msg = e.getMessage();
-        Player p = e.getPlayer();
         e.setMessage(ChatColor.translateAlternateColorCodes('&', msg));
     }
 }
